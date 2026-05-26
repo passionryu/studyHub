dbbcb01af3b07233485ca89cd9aae2f994dd9706
passionryu <rsy1225@naver.com>
2026-05-26 21:54:08 +0900

    fix: 에러 메시지를 한국말로 반환

--- apps/server/modules/application/src/main/kotlin/com/studyhub/server/application/member/DuplicateMemberEmailException.kt
+++ apps/server/modules/application/src/main/kotlin/com/studyhub/server/application/member/DuplicateMemberEmailException.kt
@@ -2,4 +2,4 @@ package com.studyhub.server.application.member
 
 class DuplicateMemberEmailException(
     val email: String,
-) : RuntimeException("Duplicated member email")
+) : RuntimeException("이미 동일한 E-mail로 등록한 회원이 있습니다.")
